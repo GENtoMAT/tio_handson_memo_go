b3f88d460d98a183909e86aba5e908e0be6cae76
GENtoMAT <maclocosmos.glandcros@gmail.com>
2024-11-06 19:10:01 +0900

    bra1

--- main.go
+++ main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	fmt.Println("start")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("successfull end")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "helloooooo")
 }
