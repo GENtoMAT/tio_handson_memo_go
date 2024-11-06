3177b5e55b09689116616561822cc7a5d14d3c81
GENtoMAT <maclocosmos.glandcros@gmail.com>
2024-11-07 04:02:02 +0900

    tio 69

--- main.go
+++ main.go
@@ -2,16 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("start")
-	http.HandleFunc("/", handler)
+	fmt.Println("serving start")
+	count = 0
+	num_access = 0
+	data, err := os.ReadFile("memo.html") //ReadFile()　でファイル読み込み。読み込み成功したらdataに渡る＆err=nilに。失敗したらerr=エラーデータ。
+	//gethtmlメソッドの中からココに移植。あそこだと関数呼ばれるたびにReadfileしてしまってる。一回読めばOK。
+	//javascriptは返り値を１つしか渡せないが、Goは複数個可能。
+	if err != nil {
+		//fmt.Fprintln(w, "can't read the file")
+		log.Fatal(err) //Fatal関数で「ココで処理を終わらせてerrを出力する」
+	}
+	htmlStr = string(data) //data変数をどこかで使わないと「定義してるが未使用」でエラーになるのでこの形に。
+
+	http.HandleFunc("/getindexhtml", getIndexHTML)
+	http.HandleFunc("/countup", countUp)
+	http.HandleFunc("/getnow", getNow)
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("successfull end")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "helloooooo")
+func getIndexHTML(w http.ResponseWriter, r *http.Request) {
+	num_access++
+	fmt.Println("access", num_access)
+	fmt.Fprintln(w, htmlStr) //dataはbyte型なのでキャストが必要
 }
+
+func countUp(w http.ResponseWriter, r *http.Request) {
+	num_access++
+	count++
+	fmt.Println("access", num_access, "countup", count)
+	fmt.Fprintln(w, count)
+}
+
+func getNow(w http.ResponseWriter, r *http.Request) {
+	num_access++
+	fmt.Println("access", num_access, "getnow", count)
+	fmt.Fprintln(w, count)
+}
+
+// 今回はココの変数で値をサーバー内に保存している。関数外で変数は宣言できるが値の代入するとエラーになる
+var count int      //countの数字
+var num_access int //アクセスされた回数
+var htmlStr string
